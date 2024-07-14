b5430908ac31a44623865ccbf6087f51e754487d
Malgrieve <theofficialmalgrieve@gmail.com>
2024-07-13 20:42:33 -0400

    C# ConsoleApp changes

--- C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ C#/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00d0f9d55e7ad76a3ff4dfa0e5147ee1fd5975f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc56f6616622c3ee9d53ec491c1b74f1f1bf3edc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
